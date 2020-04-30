b57eecc587502a86075fe2c4903437df27f9f652
David Dantas <daviddantasjunior@gmail.com>
2020-04-30 00:05:28 -0300

    Add IndexedDB with dexie

--- src/app/views/play-list/play-list.component.ts
+++ src/app/views/play-list/play-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { Movie } from 'src/app/models/movie.model';
+import { db, Movie as Mo } from 'src/app/database/appdb';
 
 @Component({
   selector: 'app-play-list',
@@ -17,6 +18,26 @@ export class PlayListComponent implements OnInit {
 
   ngOnInit(): void {
     this.movies = this.store.select('playList');
+    this.clearDb();
+    this.saveInDb();
+  }
+
+  async clearDb() {
+    try {
+      await Promise.all([db.movies.clear()]);
+      console.log("Clearing database...");
+    } catch (ex) {
+        console.error(ex);
+    }
+  }
+
+  async saveInDb() {
+    await db.transaction('rw', db.movies, async function () {
+      // Populate a contact
+      let id = await db.movies.add(new Mo('David', 'Dantas'));
+
+      console.log(`ID: ${id}`);
+    });
   }
 
 }
